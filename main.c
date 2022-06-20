25a51c2705f2096067c05a2c810e802cc955f6e7
arminevi <arminsaderi@gmail.com>
2022-06-20 12:27:14 +0200

    Added new field with 2d array

--- main.c
+++ main.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fieldOutput(int field[9][9]) {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            printf("%3i", field[i][j]);
-            if (j == 8) {
-                printf("\n");
-            }
-        }
-    }
-    return 0;
-}
+/* prints field
+*
+* @param 2d int array 
+* @return 0
+*/
+int printField(int field[9][9]) {
 
-int printField()
-{
-    char line [3] = {'+', '-', '.'};
-    char row = '|';
     // 0 = no separation line , 1 = separation line
     int separator = 0;
-    int i, j;
 
-    // row
-    for (i = 0; i < 13; i++){
+    // row 
+    for (int i = 0; i < 9; i++) {
         // indicating the 4 separation lines
         // no separation
         if (i != 0 && i != 4 && i != 8 && i != 12){
@@ -35,40 +25,38 @@ int printField()
             printf("\n");
         }
 
-        // line
-        for (j = 0; j < 25; j++){
+        // line 
+        for (int j = 0; j < 25; j++) {
             // if separation line
             if (separator == 1){
                 // print '+' every 8 character
                 if (j == 0 || j == 8 || j == 16 || j == 24){
-                    printf("%c ", line[0]); // '+'
+                    printf("+ "); // '+'
                 }else{
                     // print '-' in between
-                    printf("%c ", line[1]); // '-'
+                    printf("- "); // '-'
                 }
             }// end of separator = 1
             // NO separation line
             else if (separator == 0)
             {
-                // print the first '|' in a new line
-                j == 0 ? printf("\n%c", row) : 0 ; // '|'
-
+                // print '|' as vertical separator in a new line 
+                j == 0 ? printf("\n|") : 0 ; // '|'
+                
                 // print '|' every 8 character
-                if (j == 8 || j == 16 || j == 24){
-                    printf(" %c ", row); // '|'
+                if (j == 3 || j == 6 || j == 9){
+                    printf("|"); // '|'
                 }
-                // print '.' as placeholder. It is a 9x5 Block but for better lookings the dots have spaces in between and look like
-                else if (j == 2 || j == 4 || j == 6 || j == 10 || j == 12 || j == 14 ||j == 18 || j == 20 || j == 22)
-                {
-                    printf(" %c ", line[2]); // '.'
-                }else
-                {
-                    printf(" "); // '.'
-                }
-            }// end of separator = 0
-        }//end of for line
-    }// end of for (row)
 
+                //PRINT ARRAY 
+                if (!(j >=9) )
+                {
+                    printf("%4i ", field[i][j]);
+                }
+                
+            }
+        }
+    }
     return 0;
 }
 
@@ -85,6 +73,6 @@ int main()
         k = 1;
     }
 
-    fieldOutput(field);
+    printField(field);
     return 0;
 }
