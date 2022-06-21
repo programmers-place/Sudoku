abc7848bb091327f251e5598218b58bf2b4f2825
arminevi <arminsaderi@gmail.com>
2022-06-21 22:48:09 +0200

    Fixed field output with secondary 2d array

--- main.c
+++ main.c
@@ -6,17 +6,24 @@
 * @param 2d int array 
 * @return 0
 */
-int printField(int field[9][9]) {
-
+int printField(int field[9][9]) 
+{
+    
     // 0 = no separation line , 1 = separation line
     int separator = 0;
+    int y = 0;
+    int bool = 0;
 
     // row 
-    for (int i = 0; i < 9; i++) {
+    for (int row = 0; row < 13; row++) {
         // indicating the 4 separation lines
         // no separation
-        if (i != 0 && i != 4 && i != 8 && i != 12){
+        if (row != 0 && row != 4 && row != 8 && row != 12){
             separator = 0;
+            if (row < 12 && bool == 1){
+                y = y + 1;
+            }
+            bool = 1;
         }
         // separation
         else
@@ -26,39 +33,44 @@ int printField(int field[9][9]) {
         }
 
         // line 
-        for (int j = 0; j < 25; j++) {
+        for (int line = 0; line < 25; line++) {
             // if separation line
             if (separator == 1){
                 // print '+' every 8 character
-                if (j == 0 || j == 8 || j == 16 || j == 24){
+                if (line == 0 || line == 8 || line == 16 || line == 24){
                     printf("+ "); // '+'
                 }else{
                     // print '-' in between
                     printf("- "); // '-'
                 }
             }// end of separator = 1
-            // NO separation line
+            // NO separation line 
             else if (separator == 0)
             {
                 // print '|' as vertical separator in a new line 
-                j == 0 ? printf("\n|") : 0 ; // '|'
+                line == 0 ? printf("\n|") : 0 ; // '|'
+                if (line == 0)
+                {
+                    printf("\n|");
+                }
                 
+
                 // print '|' every 8 character
-                if (j == 3 || j == 6 || j == 9){
+                if (line == 3 || line == 6 || line == 9){
                     printf("|"); // '|'
                 }
 
-                //PRINT ARRAY 
-                if (!(j >=9) )
-                {
-                    printf("%4i ", field[i][j]);
+                if(line < 9){
+                    //DEBUG
+                    //printf("|line = %i| ", line);
+                    //printf("|y = %i| ", y);
+                    printf("%4i ", field[y][line]);
                 }
-                
-            }
-        }
-    }
+            }// end of separator = 0
+        }// end of for line 
+    }// end of for row 
     return 0;
-}
+}// end of printField
 
 int main()
 {
